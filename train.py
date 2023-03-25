2e09bb56d8f48c8fdc3986af2531a40dc7089cdd
vodxac410 <cmchik410@gmail.com>
2023-03-25 23:16:54 +0800

    update

--- train.py
+++ train.py
@@ -1,16 +1,39 @@
+import tensorflow as tf
+import glob
+import cv2
 from net.PSP import PSPnet
+from keras.optimizers import Adam
 from losses.loss import pix_acc
 
-def train(kwargs):
-    img_shape = kwargs["dimensions"] + (kwargs["channels"], )
+class trainAPI(object):
+    def __init__(self, **kwargs):
+        self.X_train = glob.glob(kwargs["train_path"])
+        self.y_train = glob.glob(kwargs["train_label_path"])
+        self.X_val = glob.glob(kwargs["val_path"])
+        self.y_val = glob.glob(kwargs["val_label_path"])
+        
+        self.img_shape = kwargs["dimensions"] + (kwargs["channels"], )
+        self.n_classes = kwargs["n_classes"]
+        self.out_ch = kwargs["ppm_output_channel"]
+        self.pool_size = kwargs["pool_size"]
+        self.strides = kwargs["strides"]
+        
+        self.batch_size = kwargs["batch_size"]
+        self.n_epochs = kwargs["epochs"]
+        self.n_steps = len(self.X_train) // self.batch_size
+        self.lr = kwargs["learning_rate"]
+        
+        self.save_model_path = kwargs["model_path"]
+         
     
-    m = PSPnet(img_shape, kwargs["n_classes"], kwargs["ppm_output_channel"], kwargs["pool_size"], kwargs["strides"])
-    
-    m.summary()
-    
-    m.compile(optimizer = "adam", loss = pix_acc)
-    
-    
+    def run(self):
+        m = PSPnet(self.img_shape, self.n_classes, self.out_ch, self.pool_size, self.strides)
+        opt = keras.optimizers.Adam()
+        for epoch in range(self.n_epochs):
+            print("Epoch {}/{}".format(epoch, self.n_epochs))
+            with tf.GradientTape() as tape:
+                y_pred
+        
 
     
 # m = PSPnet(IMG_SHAPE, N_CLASSES, OUTPUT_CHANNEL, POOL_SIZE, STRIDES)
